90cde5ae18e352a0b15578d669677c1454ba1506
Amadeo Bon <amadeosph@outlook.com>
2023-01-18 01:54:03 -0300

    Finalizado

--- scripts.js
+++ scripts.js
@@ -1,15 +1,73 @@
-function addContact() {
-    // Get the values from the form
-    let name = document.getElementById("nome-contato").value;
-    let phone = document.getElementById("phone-contato").valueAsNumber;
+const form = document.getElementById("form-atividade");
+const contato = [];
+let linhas = "";
 
-    // Create a new row for the contact
-    let table = document.getElementById("table");
-    let row = table.insertRow();
+form.addEventListener("submit", function (e) {
+  e.preventDefault();
 
-    // Insert the contact information into the cells
-    let nameCell = row.insertCell(0);
-    let phoneCell = row.insertCell(1);
-    nameCell.innerHTML = name;
-    phoneCell.innerHTML = phone;
+  adicionaLinha();
+  atualizaTabela();
+  validateNome();
+  validatePhone();
+});
+
+function adicionaLinha() {
+  const inputNome = document.getElementById("nome-contato");
+  const inputPhone = document.getElementById("phone-contato");
+  if(contato.includes(inputPhone.value)){
+    alert(`Esse telefone :${inputPhone.value} já foi inserido.`);
+    }else{
+        contato.push(inputPhone.value); 
+    }
+
+  if (contato.includes(inputNome.value)) {
+    alert(`O contato:${inputNome.value} já foi inserido.`);
+  } else {
+    contato.push(inputNome.value);
+    
+    let linha = "<tr>";
+    linha += `<td>${inputNome.value}</td>`;
+    linha += `<td>${inputPhone.value}</td>`;
+    linha += `</tr>`;
+
+    linhas += linha;
   }
+
+  inputNome.value = "";
+  inputPhone.value = "";
+}
+
+function atualizaTabela() {
+  const corpoTabela = document.querySelector("tbody");
+  corpoTabela.innerHTML = linhas;
+}
+
+//Validação
+const nomeErro = document.getElementById("nome-erro");
+const phoneErro = document.getElementById("phone-erro");
+
+function validateNome() {
+  let name = document.getElementById("nome-contato").value;
+
+  if (name.length == 0) {
+    nomeErro.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+    return false;
+  }
+  nomeErro.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+function validatePhone() {
+  let phone = document.getElementById("phone-contato").value;
+
+  if (phone.length == 0) {
+    phoneErro.innerHTML = '<i class="fa-solid fa-circle-xmark"></i>';
+    return false;
+  }
+  phoneErro.innerHTML = '<i class="fa-solid fa-circle-check"></i>';
+  return true;
+}
+const inputNome = document.getElementById("nome-contato");
+
+inputNome.addEventListener("input", function (e) {
+    this.value = this.value.replace(/[^a-zA-Z]/g, '');
+});
